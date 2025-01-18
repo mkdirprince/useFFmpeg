2ec0113c49fa1e8ec00a88c1e7f8c08dc6eb3299
mkdirprince <mkdirprince@github.com>
2025-01-18 00:55:08 +0000

    fix: resolve React type import error in FFmpegContext

--- src/context/FFmpegContext.tsx
+++ src/context/FFmpegContext.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React from "react";
 import { createContext, useContext } from "react";
 import { useFFmpeg as useFFmpegBase } from "../hooks/useFFmpeg";
 import type { UseFFmpeg } from "../types/useFFmpeg";
